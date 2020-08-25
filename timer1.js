e0615224ee7923f1425c75b7fdcf179aa310db64
ahmedalwardani <aalwa049@uottawa.ca>
2020-08-24 20:37:26 -0400

    adjust conditional

--- timer1.js
+++ timer1.js
@@ -1,17 +1,42 @@
-const timer = time => {
-  setTimeout(() => {
-    process.stdout.write("working...");
-  }, time);
+// const timer = time => {
+//   setTimeout(() => {
+//     process.stdout.write("working...\n");
+//   }, time);
+// };
+
+// const array = process.argv.slice(2);
+// const arrayNumbers = array.map(element => parseInt(element));
+
+
+// for (const elements of arrayNumbers) {
+//   if (typeof elements === "number" && elements >= 0) {
+//     timer(elements * 1000);
+//   }
+// }
+
+
+const beep = function() {
+  process.stdout.write("\x07");
+  console.log("beeep");
 };
 
-const array = process.argv.slice(2);
-const arrayNumbers = array.map(element => parseInt(element));
+// delay is expected in seconds
+const scheduleBeep = function(delay) {
+  // if (delay < 0 || isNaN(delay)) {
+  //   return;
+  // }
 
+  delay = Number(delay);
 
-for (const elements of arrayNumbers) {
-  if (typeof elements === "number" && elements >= 0) {
-    timer(elements * 1000);
+  // setTimeout(beep, delay * 1000);
+
+  if (!isNaN(delay) && delay >= 0) {
+    setTimeout(beep, delay * 1000);
   }
-}
+};
 
+const numbers = process.argv.slice(2);
+for (const num of numbers) {
+  scheduleBeep(num);
+}
 
